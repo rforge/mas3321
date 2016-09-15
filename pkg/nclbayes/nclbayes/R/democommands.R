b57bcdf7bfb541bb17595394a3a277298bcc069f
nclbayes <nclbayes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-15 09:39:04 +0000

    2016-09-15
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mas3321@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nclbayes/nclbayes/R/democommands.R
+++
@@ -1,9 +0,0 @@
-#' Launches an editor containing the commands in a demo
-#' @param demo name of demo.
-#' @importFrom utils file.edit
-#' @export
-demoCommands=function(demo){
-  filename = paste(deparse(substitute(demo)),".R",sep="")
-  pathname = system.file("demo",filename, package="nclbayes")
-  file.edit(pathname,title = filename)
-}
