f4a6e0094e2757dc386939d7ca7bc79455bfd02a
nclbayes <nclbayes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-09-21 15:37:01 +0000

    2016-09-21
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mas3321@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nclbayes/R/data_files.R
+++ pkg/nclbayes/R/data_files.R
@@ -1,4 +1,4 @@
-#' Data collected by the 18th century physicist Henry Cavendish made 23 experimental
+#' Data collected by the 18th century physicist Henry Cavendish, who made 23 experimental
 #' determinations of the earth's density (in g per cubic cm)
 #' @name cavendish
 #' @docType data
@@ -14,10 +14,10 @@ NULL
 #' @keywords datasets
 NULL
 
-#' Data on the aerobic colony counts (acc, measured in colony forming
-#' units per millilitre) of ten selected keyboards with samples taken
-#' from four keys per keyboard on a particular day.  The keyboards were
-#' selected randomly from those available in the computer laboratory.
+#' Data on the aerobic colony counts (acc, measured in colony forming units per
+#' millilitre) of ten selected keyboards with samples taken from four keys per
+#' keyboard on a particular day. The keyboards were selected randomly from those
+#' available in the computer laboratory.
 #' @name contamination
 #' @docType data
 #' @return A data frame with 40 rows and 2 variables.
@@ -49,14 +49,19 @@ NULL
 #' @keywords datasets
 NULL
 
-#' Data on abrasion
+#' Data from an experiment to investigate how weight loss due to abrasion (in g/hr) of rubber
+#' is affected by hardness (in degrees Shore) and tensile strength (in kg/cm^2).
 #' @name abrasion
 #' @docType data
+#' @references This data set was taken from Davies and Goldsmith, Statistical Methods in
+#' Research and Production, 4th edition.
 #' @return A data frame with 30 rows and 3 variables.
 #' @keywords datasets
 NULL
 
-#' Data on insulation
+#' Data on weekly gas consumption (in thousands of cubic feet) at 30 houses over 56 weeks.
+#' The data includes the outside temperature (in degrees C), and the presence of insulation
+#' (z=1: no insulation, z=2: insulation present).
 #' @name insulation
 #' @docType data
 #' @return A data frame with 56 rows and 32 variables.
@@ -70,38 +75,48 @@ NULL
 #' @keywords datasets
 NULL
 
-#' Data on protein
+#' Data from an experiment to calibrate a near infrared reflectance instrument. Included is
+#' the protein content (\%) as determined by a standard method (y1 to y30), and measurements
+#' of reflectance at six different wavelengths (x1 to x6).
 #' @name protein
 #' @docType data
 #' @return A data frame with 24 rows and 36 variables.
 #' @keywords datasets
 NULL
 
-#' Data on sales
+#' Data on the number of orders received by a small company for one of its products over time.
+#' The data consists of 30 sets of 100 consecutive weeks.
 #' @name sales
 #' @docType data
-#' @return A data frame with 100 rows and 30 variables.
+#' @return A list containing 30 variables with 100 rows, and a variable with 1 row.
 #' @keywords datasets
 NULL
 
-#' Data on IBM prices
+#' Data consisting of 160 consecutive daily IBM common stock closing prices.
 #' @name IBMprices
 #' @docType data
-#' @return A data frame with 160 rows with 1 variable.
+#' @references Box and Jenkins, Time Series Analysis, 1976.
+#' @return A list containing a variable with 160 rows, and a variable with 1 row.
 #' @keywords datasets
 NULL
 
-#' Data on surgery
+#' Data on the number of patients undergoing surgery and the number who died in hospital
+#' in two areas of the USA, broken down by age and sex. A group number corresponding to
+#' area and sex is provided, along with the midpoint of the age range (in years).
 #' @name surgery
 #' @docType data
 #' @return A data frame with 32 rows with 4 variables.
+#' @references Mosteller and Tukey, Data Analysis and Regression, 1977.
 #' @keywords datasets
 NULL
 
-#' Data on old faithful
+#' Data on successive waiting times (in minutes) between eruptions of the "Old Faithful"
+#' geyser in Yellowstone National Park, Wyoming, USA.
 #' @name oldfaithful
 #' @docType data
-#' @return A data frame with 299 rows with 1 variable.
+#' @references Azzalini and Bowman, A look at some data on the Old Faithful geyser,
+#' Journal of the Royal Statistical Society, Series C, 39, 357-366, (1990).
+#' @return A list containing a variable with 299 rows, and a variable with 1 row.
 #' @keywords datasets
 NULL
 
@@ -112,17 +127,14 @@ NULL
 #' @keywords datasets
 NULL
 
-#' Data on babies
+#'Data showing the outcomes from a set of babies, categorised as 'Full term' or
+#''Premature', and 'alive at end of year 1' or 'died in year 1'.
+#'The mothers are included, categorised as either 'Young' or 'Older' and 'Smokers'
+#'or 'Non-smokers'.
 #' @name babies
 #' @docType data
-#' @return A list with 1 matrix and 3 variables.
-#' @keywords datasets
-NULL
-
-#' Data on old faithful
-#' @name oldfaithful
-#' @docType data
-#' @return A data frame with 299 rows with 1 variable.
+#' @references Freeman, Applied Categorical Data Analysis, 1987.
+#' @return A list containing a 4x4 matrix and 3 variables with 4 rows.
 #' @keywords datasets
 NULL
 
@@ -133,17 +145,22 @@ NULL
 #' @keywords datasets
 NULL
 
-#' Data on side effects
+#' Data showing the number of patients who were given different doses of a
+#' certain drug, along with the number of patients who exhibited a
+#' particular side effect.
 #' @name sideeffects
 #' @docType data
 #' @return A data frame with 7 rows with 3 variables.
 #' @keywords datasets
 NULL
 
-#' Data on aircraft
+#'Data on the number of 'multi-fatality crashes' in the week following 17 known cases of
+#''murder-suicide', together with an index of newspaper coverage.
 #' @name aircraft
+#' @references Phillips, Airplane accident fatalities increase just after newspaper
+#' stories about murder and suicide, Science, 201, 748-750, (1978).
 #' @docType data
-#' @return A data frame with 17 rows with 2 variables.
+#' @return A list containing 2 variables with 17 rows, and a variable with 1 row.
 #' @keywords datasets
 NULL
 
