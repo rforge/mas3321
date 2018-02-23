177dfd4f79e0257924fa98f75247c59726c47df3
nclbayes <nclbayes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-02-23 10:14:17 +0000

    Convert columns in movies to be of correct types
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mas3321@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ncldata/R/movies.R
+++ pkg/ncldata/R/movies.R
@@ -32,3 +32,105 @@
 #' @examples
 #' data(movies)
 "movies"
+
+# FURTHER INFO
+# ---------------------------------------------
+# There are two sources of data, the first provides .tsv formatted files: https://datasets.imdbws.com/
+# The second provides .list files: ftp://ftp.fu-berlin.de/pub/misc/movies/database/frozendata/
+#
+# The .list files were converted to .tsv with:
+#
+# gawk -v RS='-------------------------------------------------------------------------------'
+# -v FS="\n" '{printf "%s\t",gensub("MV: | \\([0-9]+.*\\)","","g",$2);printf "%s\t",gensub(/.*
+# ([0-9][0-9][0-9][0-9]).*/,"\\1","g",$2);for(j=3;j<=NF;j++){if($j ~ /RE: [rR]ated/) {printf
+# "%s",gensub("RE: [rR]ated ([A-Z]+-*?[0-9]*?) .*","\\1","g",$j)}};print " ";}' mpaa-ratings-reasons.list
+# | sed 's/"//g' | sed 's/"//g' > mpaa.tsv
+#
+# and
+#
+# gawk -v RS='-------------------------------------------------------------------------------'
+# -v FS="\n" '{printf "%s\t",gensub("MV: |BT:| \\([0-9]+.*\\)","","g",$2);printf "%s\t",gensub(/.*
+# ([0-9][0-9][0-9][0-9]).*/,"\\1","g",$2);for(j=3;j<=NF;j++){if($j ~ /BT:/) {printf "%s",gensub
+# ("MV: |BT: | \\([0-9]+.*\\)","","g",$j)}};print " ";}' business.list | sed 's#"##g' > budget.tsv
+#
+# The headers in budget.tsv and mpaa.tsv were inserted by hand.
+# Approximately 10 rows with bad data were corrected by hand.
+#
+# The data was imported with something like the following R commands:
+#
+#  imdbtitles = read.table(file = "title.basics.tsv", header=T, sep="\t",quote="",comment.char="",fileEncoding="utf-8")
+#  imdbratings = read.table(file = "title.ratings.tsv", header=T, sep="\t",quote="",comment.char="",fileEncoding="utf-8")
+#  imdbbudget = read.table(file = "budget.tsv", header=T, sep="\t",quote="",comment.char="",fileEncoding="iso-8859-13")
+#  imdbmpaa = read.table(file = "mpaa.tsv", header=T, sep="\t",quote="",comment.char="",fileEncoding="iso-8859-13")
+#  imdbdata = merge(imdbtitles,imdbratings,by = "tconst")
+#  m=20000
+#  imdbclean = imdbdata[which(imdbdata$isAdult == 0 & imdbdata$titleType=="movie" & imdbdata$numVotes > m),]
+#  c = mean(imdbclean$averageRating)
+#  imdbwr = (imdbclean$numVotes / (imdbclean$numVotes+m)) * imdbclean$averageRating + (m / (imdbclean$numVotes+m)) * c
+#  imdbclean = imdbclean[order(imdbclean$primaryTitle),]
+#
+#  imdbclean = merge(imdbclean, imdbbudget,all.x=T,all.y=F)
+#  imdbclean=imdbclean[!duplicated(imdbclean$tconst), ]
+#  imdbclean$BudgetValue = strtoi(gsub(",", "", sub(".*?([0-9,]+).*","\\1",imdbclean$Budget)))
+#  imdbclean$BudgetCurrency = sub("([A-z]*).*","\\1",imdbclean$Budget)
+#  imdbclean$BudgetCurrency[which(imdbclean$BudgetCurrency=="")]=NA
+#
+#  imdbmpaa = read.table(file = "mpaa.tsv", header=T, sep="\t",quote="",comment.char="",fileEncoding="iso-8859-13")
+#  imdbclean = merge(imdbclean, imdbmpaa,all.x=T,all.y=F)
+#  imdbclean=imdbclean[!duplicated(imdbclean$tconst), ]
+#
+#  movies_new = data.frame(Title=imdbclean$primaryTitle,Year=imdbclean$startYear,
+#                          Rating=imdbclean$averageRating,Votes=imdbclean$numVotes,
+#                          Length=imdbclean$runtimeMinutes,
+#                          Budget=imdbclean$BudgetValue,
+#                          BudgetCurrency=imdbclean$BudgetCurrency,
+#                          mpaa=imdbclean$mpaa,
+#                          Action=grepl("Action",imdbclean$genres),
+#                          Adventure=grepl("Adventure",imdbclean$genres),
+#                          Animation=grepl("Animation",imdbclean$genres),
+#                          Biography=grepl("Biography",imdbclean$genres),
+#                          Comedy=grepl("Comedy",imdbclean$genres),
+#                          Crime=grepl("Crime",imdbclean$genres),
+#                          Documentary=grepl("Documentary",imdbclean$genres),
+#                          Drama=grepl("Drama",imdbclean$genres),
+#                          Fantasy=grepl("Fantasy",imdbclean$genres),
+#                          Horror=grepl("Horror",imdbclean$genres),
+#                          Mystery=grepl("Mystery",imdbclean$genres),
+#                          Romance=grepl("Romance",imdbclean$genres),
+#                          SciFi=grepl("Sci-Fi",imdbclean$genres),
+#                          Short=grepl("Short",imdbclean$genres),
+#                          Thriller=grepl("Thriller",imdbclean$genres))
+#
+#  movies <- movies_new %>%
+#    mutate_if(is.character,
+#              stri_trans_general,
+#              id = "latin-ascii")
+#
+#  movies$mpaa[which(movies$mpaa == "NC-17 ")] = "NC-17"
+#  movies$mpaa[which(movies$mpaa == "PG ")] = "PG"
+#  movies$mpaa[which(movies$mpaa == "PG-13 ")] = "PG-13"
+#  movies$mpaa[which(movies$mpaa == "R ")] = "R"
+#  movies$mpaa[which(movies$mpaa == " ")] = NA
+#
+#  movies$Year = as.numeric(movies$Year)
+#  movies$Rating = as.numeric(movies$Rating)
+#  movies$Votes = as.numeric(movies$Votes)
+#  movies$Length = as.numeric(movies$Length)
+#  movies$Budget = as.numeric(movies$Budget)
+#  movies$BudgetCurrency = as.factor(movies$BudgetCurrency)
+#  movies$mpaa = as.factor(movies$mpaa)
+#  movies$Action = as.logical(movies$Action)
+#  movies$Adventure = as.logical(movies$Adventure)
+#  movies$Animation = as.logical(movies$Animation)
+#  movies$Biography = as.logical(movies$Biography)
+#  movies$Comedy = as.logical(movies$Comedy)
+#  movies$Crime = as.logical(movies$Crime)
+#  movies$Documentary = as.logical(movies$Documentary)
+#  movies$Drama = as.logical(movies$Drama)
+#  movies$Fantasy = as.logical(movies$Fantasy)
+#  movies$Horror = as.logical(movies$Horror)
+#  movies$Mystery = as.logical(movies$Mystery)
+#  movies$Romance = as.logical(movies$Romance)
+#  movies$SciFi = as.logical(movies$SciFi)
+#  movies$Short = as.logical(movies$Short)
+#  movies$Thriller = as.logical(movies$Thriller)
